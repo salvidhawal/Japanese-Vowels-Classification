b7ff1774ab7724326c43dae895493736f3a95055
salvidhawal <salvidhawal@gmail.com>
2021-12-14 17:01:48 +0100

    craeted general_code folder for placing simple code

--- general_code/read_the_individual.py
+++ general_code/read_the_individual.py
@@ -10,4 +10,5 @@ for i in range(rows):
     if ae_train[i,0] == 1.0:
         count += 1
 
-print(f"total off 1: {count}")
+print(f"total of 1: {count}")
+print(f"total dataset for 1 person: {count/9}")
