4b389694742600c9ab4c386e9dd52f2961864c0c
salvidhawal <salvidhawal@gmail.com>
2021-12-29 17:25:31 +0100

    completed svm with kfold

--- general_code/convert_data_tonumpy.py
+++ general_code/convert_data_tonumpy.py
@@ -1,10 +1,50 @@
 import numpy as np
 import itertools
 
+
+def save_dataset_in_numpy(X, blockLengthes):
+    X_rows, X_col = X.shape
+    y = []
+    class_counter = 1
+    increment_counter = 0
+    j = 0
+    for i in range(X_rows):
+        if X[i][0] == 1.0 and X[i][11] == 1.0:
+            y.append(0)
+            increment_counter += 1
+        else:
+            y.append(class_counter)
+
+        if increment_counter == blockLengthes[j]:
+            increment_counter = 0
+            class_counter += 1
+            j += 1
+
+    X = np.delete(X, np.where((X == 1.0))[0], axis=0)
+    print(X.shape)
+
+    y = np.array(y)
+    y = np.delete(y, np.where((y == 0.0))[0], axis=0)
+    print(y.shape)
+    return X, y
+
+
 filename = ["ae.train", "ae.test"]
 savename = ["ae_train", "ae_test"]
 
 for file, savefile in itertools.zip_longest(filename, savename):
-    File_data = np.loadtxt(f"dataset/{file}", dtype=float)
+    File_data = np.loadtxt(f"../dataset/{file}", dtype=float)
     print(File_data.shape)
-    np.save(f"dataset/{savefile}", File_data)
+    np.save(f"../dataset/{savefile}", File_data)
+
+ae_train = np.load("../dataset/ae_train.npy")
+blockLengthes = [30, 30, 30, 30, 30, 30, 30, 30, 30]
+X_train, y_train = save_dataset_in_numpy(X=ae_train, blockLengthes=blockLengthes)
+np.save(f"../dataset/X_train", X_train)
+np.save(f"../dataset/y_train", y_train)
+
+ae_test = np.load("../dataset/ae_test.npy")
+blockLengthes_test = [31, 35, 88, 44, 29, 24, 40, 50, 29]
+X_test, y_test = save_dataset_in_numpy(X=ae_test, blockLengthes=blockLengthes_test)
+np.save(f"../dataset/X_test", X_test)
+np.save(f"../dataset/y_test", y_test)
--- main_code/grouping_data.py
+++ main_code/grouping_data.py
@@ -1,73 +1,24 @@
 import numpy as np
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_validate, RepeatedKFold
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
-    ae_train = np.load("../dataset/ae_train.npy")
-    ae_test = np.load("../dataset/ae_test.npy")
-    print(ae_train.shape)
-    print(ae_test.shape)
-    ae_train_rows, ae_train_col = ae_train.shape
-    ae_test_rows, ae_test_col = ae_test.shape
+    X_train = np.load(f"../dataset/X_train.npy")
+    Y_train = np.load(f"../dataset/y_train.npy")
+    X_test = np.load(f"../dataset/X_test.npy")
+    Y_test = np.load(f"../dataset/y_test.npy")
 
-    X_train = np.delete(ae_train, np.where((ae_train == 1.0))[0], axis=0)
-    print(X_train.shape)
-    X_test = np.delete(ae_test, np.where((ae_test == 1.0))[0], axis=0)
-    print(X_test.shape)
+    X, Y = shuffle(X_train, Y_train, random_state=0)
+    kf = KFold(n_splits=5, shuffle=False)
+    rkf = RepeatedKFold(n_splits=5, n_repeats=2, random_state=42)
+    clf = SVC(kernel='linear', C=1, random_state=2)
 
-    y_train = []
-    class_counter = 1
-    increment_counter = 0
-    for i in range(ae_train_rows):
-        if ae_train[i][0] == 1.0 and ae_train[i][11] == 1.0:
-            y_train.append(0)
-            increment_counter += 1
-        else:
-            y_train.append(class_counter)
+    cv_results = cross_validate(clf, X, Y, cv=rkf, return_estimator=True)
+    print(f"validation accuracy after kfold: {cv_results['test_score']}")
+    max_accuracy_index = np.argmax(cv_results["test_score"])
+    rfc_fit = cv_results['estimator']
 
-        if increment_counter == 30:
-            increment_counter = 0
-            class_counter += 1
-
-    blockLengthes = [31, 35, 88, 44, 29, 24, 40, 50, 29]
-    y_test = []
-    class_counter = 1
-    increment_counter = 0
-    j = 0
-    for i in range(ae_test_rows):
-        if ae_test[i][0] == 1.0 and ae_test[i][11] == 1.0:
-            y_test.append(0)
-            increment_counter += 1
-        else:
-            y_test.append(class_counter)
-
-        if increment_counter == blockLengthes[j]:
-            print(blockLengthes[j])
-            increment_counter = 0
-            class_counter += 1
-            j = j+1
-
-    y_train = np.array(y_train)
-    y_train = np.delete(y_train, np.where((y_train == 0.0))[0], axis=0)
-    print(y_train.shape)
-
-    y_test = np.array(y_test)
-    y_test = np.delete(y_test, np.where((y_test == 0.0))[0], axis=0)
-    print(y_test.shape)
-
-    X, y = shuffle(X_train, y_train)
-    x_train, x_val, y_train, y_val = train_test_split(X, y, test_size=0.1)
-
-    clf = SVC(kernel='linear')
-    clf.fit(x_train, y_train)
-
-    y_pred_val = clf.predict(x_val)
-    print(accuracy_score(y_val, y_pred_val))
-
-    y_pred = clf.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
-
-
-
+    y_pred = rfc_fit[max_accuracy_index].predict(X_test)
+    print(f"test dataset accuracy with best svm model: {accuracy_score(Y_test, y_pred)}")
